c5dfce412328fc738e1938f07e85a6e22bace510
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2024-10-25 21:01:00 +0800

    Created Question Model and Migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,11 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
     /**
      * Get the attributes that should be cast.
      *
