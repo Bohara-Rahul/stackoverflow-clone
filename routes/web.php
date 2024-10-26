27ce8719babba519d066e5d3db594ddfc0ee701f
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2024-10-26 17:12:00 +0800

    display all questions done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\QuestionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('questions', QuestionsController::class);
